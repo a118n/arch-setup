0bb0eb5a81f66ae6230c1b54ba3d015ffb3c1e1d
Daniel Allen <a118n@users.noreply.github.com>
2023-10-17 13:37:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,28 +26,22 @@ swapon /dev/nvme1n1p2
 reflector --verbose --protocol https --connection-timeout 2 --download-timeout 2 --score 0 --latest 50 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Base
-pacstrap /mnt base base-devel linux linux-rt linux-firmware grub os-prober efibootmgr amd-ucode bash-completion bluez curl git htop lsof man-db man-pages networkmanager ntfs-3g texinfo tree vim
+pacstrap /mnt base base-devel linux linux-rt linux-firmware grub efibootmgr amd-ucode bash-completion bluez curl wget jq git htop lsof man-db man-pages networkmanager ntfs-3g texinfo tree vim
 
 # Video
-pacstrap /mnt libva-mesa-driver mesa mesa-vdpau vulkan-radeon xf86-video-amdgpu libva-mesa-driver mesa-vdpau vulkan-tools
+pacstrap /mnt mesa mesa-vdpau vulkan-radeon libva-mesa-driver mesa-vdpau vulkan-tools #xf86-video-amdgpu
 
 # GNOME
-pacstrap /mnt baobab eog evince file-roller gdm gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-keyring gnome-remote-desktop gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-tweaks gnome-user-share gnome-weather gvfs gvfs-mtp gvfs-smb lollypop nautilus power-profiles-daemon sushi transmission-gtk xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs-gtk
+# pacstrap /mnt baobab eog evince file-roller gdm gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-clocks gnome-control-center gnome-disk-utility gnome-keyring gnome-remote-desktop gnome-screenshot gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-tweaks gnome-user-share gnome-weather gvfs gvfs-mtp gvfs-smb lollypop nautilus power-profiles-daemon sushi transmission-gtk xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs-gtk
 
 # KDE
-# pacstrap /mnt plasma plasma-wayland-session sddm sddm-kcm dolphin konsole kate ark p7zip unrar ffmpegthumbs filelight gwenview qt5-imageformats kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdeplasma-addons kdialog okular partitionmanager samba spectacle
+pacstrap /mnt plasma plasma-wayland-session sddm sddm-kcm dolphin konsole kate ark p7zip unrar ffmpegthumbs filelight gwenview qt5-imageformats kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdeplasma-addons kdialog okular partitionmanager samba spectacle
 
 # Multimedia
-pacstrap -i /mnt mpv pipewire-alsa pipewire-jack pipewire-pulse youtube-dl
-
-# Gaming
-pacstrap -i /mnt steam discord
+pacstrap -i /mnt mpv pipewire-alsa pipewire-jack pipewire-pulse
 
 # Containers & Virtualization
-pacstrap -i /mnt bridge-utils dnsmasq docker edk2-ovmf iptables-nft libvirt qemu virt-manager
-
-# Ansible
-pacstrap -i /mnt ansible ansible-lint yamllint
+pacstrap -i /mnt bridge-utils dnsmasq docker edk2-ovmf iptables-nft libvirt qemu-base virt-manager
 
 # Misc
 pacstrap /mnt terminus-font ttf-cascadia-code firefox
@@ -59,7 +53,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sed -i 's/#Color/Color/' /etc/pacman.conf
 arch-chroot /mnt sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 arch-chroot /mnt sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
-arch-chroot /mnt sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf # Enable Multilib
+# arch-chroot /mnt sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf # Enable Multilib
 
 # Configure timezone and locale
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -71,10 +65,10 @@ echo "LANG=en_US.UTF-8"  > /mnt/etc/locale.conf
 echo "FONT=ter-132n" > /mnt/etc/vconsole.conf
 
 # Configure hostname
-echo "Obelisk" > /mnt/etc/hostname
+echo "obelisk" > /mnt/etc/hostname
 echo "127.0.0.1    localhost" >> /mnt/etc/hosts
 echo "::1    localhost" >> /mnt/etc/hosts
-echo "127.0.1.1    Obelisk.local    Obelisk" >> /mnt/etc/hosts
+echo "127.0.1.1    obelisk.local    obelisk" >> /mnt/etc/hosts
 
 # Install GRUB
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="Arch Linux"
@@ -85,12 +79,13 @@ arch-chroot /mnt sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& amdgpu.ppfeaturem
 # Configure GRUB resolution
 arch-chroot /mnt sed -i 's/GRUB_GFXMODE=auto/GRUB_GFXMODE=2560x1440x32,1920x1080x32,auto/' /etc/default/grub
 # Enable os-prober to detect other OSes
-arch-chroot /mnt echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+# arch-chroot /mnt echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Fix GDM not starting after boot (Enable early KMS)
-arch-chroot /mnt sed -i 's/^MODULES=([^)]*/&amdgpu/' /etc/mkinitcpio.conf
+# arch-chroot /mnt sed -i 's/^MODULES=([^)]*/&amdgpu/' /etc/mkinitcpio.conf
+
 arch-chroot /mnt mkinitcpio -P
 
 # Add user and change passwords
@@ -106,8 +101,9 @@ arch-chroot /mnt systemctl enable fstrim.timer
 
 # Enable necessary services
 arch-chroot /mnt systemctl enable NetworkManager.service
-arch-chroot /mnt systemctl enable gdm.service
-# arch-chroot /mnt systemctl enable sddm.service
+arch-chroot /mnt systemctl enable systemd-resolved.service
+# arch-chroot /mnt systemctl enable gdm.service
+arch-chroot /mnt systemctl enable sddm.service
 arch-chroot /mnt systemctl enable bluetooth.service
 arch-chroot /mnt systemctl enable libvirtd.service
 arch-chroot /mnt systemctl enable docker.service
